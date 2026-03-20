2cef4d5433a4ee3255471a3c937cd18cd1960c45
MARJORIE MANALO <mrbmanalo@paterostechnologicalcollege.edu.ph>
2026-03-20 18:34:46 +0800

    Create grp3_manalo_ex6.py

---
+++ grp3_manalo_ex6.py
@@ -0,0 +1,30 @@
+# IF STATEMENT (returns True or False)
+
+age = 18
+
+is_adult = age >= 18
+print("Is the person an adult?", is_adult)
+
+
+# LIST (String, int, boolean)
+my_list = ["Apple", 10, True]
+print("\nList:", my_list)
+
+
+# TUPLE (String, int, boolean)
+my_tuple = ("Banana", 20, False)
+print("Tuple:", my_tuple)
+
+
+# SET (String, int, boolean)
+my_set = {"Orange", 30, True}
+print("Set:", my_set)
+
+
+# DICTIONARY (String, int, boolean)
+my_dict = {
+    "fruit": "Mango",     # string
+    "quantity": 15,       # int
+    "available": True     # boolean
+}
+print("Dictionary:", my_dict)
